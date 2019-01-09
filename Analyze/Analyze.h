72ce257bf6d470a64a58ad5f862fc951f5f3a8cc
Kyle Begeman <kyle.begeman@leafly.com>
2019-01-09 14:43:11 -0800

    Initial Commit

---
+++ Analyze/Analyze.h
@@ -0,0 +1,19 @@
+//
+//  Analyze.h
+//  Analyze
+//
+//  Created by Kyle Begeman on 1/9/19.
+//  Copyright © 2019 Kyle Begeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Analyze.
+FOUNDATION_EXPORT double AnalyzeVersionNumber;
+
+//! Project version string for Analyze.
+FOUNDATION_EXPORT const unsigned char AnalyzeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Analyze/PublicHeader.h>
+
+
